50fe00571db69513e9592681524ffacce753c186
Felipe <felipe.brum4@gmail.com>
2025-04-14 11:41:00 -0300

    Atualizando arquivo .asm

--- Projeto.asm
+++ Projeto.asm
@@ -1,8 +1,11 @@
-ORG 0000H
+RS equ P1.3 
+EN equ P1.2
 
+org 0000h
+	LJMP INICIO
+
+org 0100h
 INICIO:
-ORG 0100H
-
 
 ;DB de respostas
 msg_hora:DB "Hora Atual:", 0
@@ -31,3 +34,170 @@ msg_ola2:       DB "Posso ajudar?", 0
 
 msg_erro:       DB "Não entendi!!", 0
 msg_erro2:      DB "Fale novamente", 0
+
+
+
+;Sub-Rotinas -> Display
+lcd_init:
+	CLR RS		
+
+	CLR P1.7		
+	CLR P1.6		
+	SETB P1.5		
+	CLR P1.4		
+
+	SETB EN		
+	CLR EN		
+
+	CALL delay		
+
+	SETB EN		
+	CLR EN		
+
+	SETB P1.7		
+
+	SETB EN		
+	CLR EN		
+
+	CALL delay		
+
+
+	CLR P1.7		
+	CLR P1.6		
+	CLR P1.5		
+	CLR P1.4		
+
+	SETB EN		
+	CLR EN		
+
+	SETB P1.6		
+	SETB P1.5		
+
+	SETB EN		
+	CLR EN		
+
+	CALL delay		
+
+
+	CLR P1.7		
+	CLR P1.6		
+	CLR P1.5		
+	CLR P1.4		
+
+	SETB EN		
+	CLR EN		
+
+	SETB P1.7		
+	SETB P1.6		
+	SETB P1.5		
+	SETB P1.4		
+
+	SETB EN		
+	CLR EN		
+
+	CALL delay		
+	RET
+
+sendCharacter:
+	SETB RS  		
+	MOV C, ACC.7		
+	MOV P1.7, C		
+	MOV C, ACC.6		
+	MOV P1.6, C		
+	MOV C, ACC.5		
+	MOV P1.5, C		
+	MOV C, ACC.4		
+	MOV P1.4, C		
+
+	SETB EN		
+	CLR EN		
+
+	MOV C, ACC.3		
+	MOV P1.7, C		
+	MOV C, ACC.2		
+	MOV P1.6, C		
+	MOV C, ACC.1		
+	MOV P1.5, C		
+	MOV C, ACC.0		
+	MOV P1.4, C		
+
+	SETB EN		
+	CLR EN		
+
+	CALL delay		
+	RET
+
+posicionaCursor:
+	CLR RS	         
+	SETB P1.7		
+	MOV C, ACC.6		
+	MOV P1.6, C		
+	MOV C, ACC.5		
+	MOV P1.5, C		
+	MOV C, ACC.4		
+	MOV P1.4, C		
+
+	SETB EN		
+	CLR EN		
+
+	MOV C, ACC.3		
+	MOV P1.7, C		
+	MOV C, ACC.2		
+	MOV P1.6, C		
+	MOV C, ACC.1		
+	MOV P1.5, C		
+	MOV C, ACC.0		
+	MOV P1.4, C		
+
+	SETB EN		
+	CLR EN		
+
+	CALL delay		
+	RET
+
+retornaCursor:
+	CLR RS	      
+	CLR P1.7		
+	CLR P1.6		
+	CLR P1.5		
+	CLR P1.4		
+
+	SETB EN		
+	CLR EN		
+
+	CLR P1.7		
+	CLR P1.6		
+	SETB P1.5		
+	SETB P1.4		
+
+	SETB EN		
+	CLR EN		
+
+	CALL delay		
+	RET
+
+clearDisplay:
+	CLR RS	      
+	CLR P1.7		
+	CLR P1.6		
+	CLR P1.5		
+	CLR P1.4		
+
+	SETB EN		
+	CLR EN		
+
+	CLR P1.7		
+	CLR P1.6		
+	CLR P1.5		
+	SETB P1.4		
+
+	SETB EN		
+	CLR EN		
+
+	CALL delay		
+	RET
+
+delay:
+	MOV R0, #50
+	DJNZ R0, $
+	RET
