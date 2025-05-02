7d08b7269718cfd39000bf0477f100aedeb218fc
Felipe <felipe.brum4@gmail.com>
2025-05-02 18:46:44 -0300

    finalizando o código

--- Projeto.asm
+++ Projeto.asm
@@ -1,9 +1,9 @@
+;Obs: Para o projeto rodar, precisamos mudar os pinos do LED para p0
+
 RS equ P1.3 
 EN equ P1.2
-
 ORG 0000h
 	LJMP INICIO
-
 ORG 0100h
 
 INICIO:
@@ -15,12 +15,12 @@ INICIO:
 	MOV IE,#90H ; Habilita interrupção serial
 	SETB TR1
 	MOV R0, #30H
+	MOV R1, #29H
 
 SALVAR_ACAO:
 	JNB RI, SALVAR_ACAO
 	CLR RI
 	MOV A, SBUF
-    
 	; Veririca se são caracteres de validacao do ASCII como enters e /r
 	CJNE A, #0DH, VERIFICA_LF
 	SJMP SALVAR_ACAO    
@@ -29,7 +29,7 @@ VERIFICA_LF:
 	; Verifica se é LF (0A)
 	CJNE A, #0AH, SALVAR_CARACTERE
 	SJMP SALVAR_ACAO    ; Ignora LF e volta para esperar próximo caractere
-    
+
 SALVAR_CARACTERE:
 	MOV @R0, A
 	CJNE A, #'$', CONTINUAR
@@ -41,10 +41,236 @@ CONTINUAR:
 
 FIM:
 	MOV R0, #30H
+
 	MOV DPTR, #msg_horas
 	ACALL COMPARAR_STRING
 	JZ HORAS
+
+	MOV DPTR, #msg_ligar_luz
+	ACALL COMPARAR_STRING
+	JZ LUZ
+
+	MOV DPTR, #msg_apagar_luz
+	ACALL COMPARAR_STRING
+	JZ APAGAR_LUZ
+
+	MOV DPTR, #msg_repouso_pergunta
+	ACALL COMPARAR_STRING
+	JZ FLAG_REPOUSO
+
+	MOV DPTR, #msg_reiniciar3
+	ACALL COMPARAR_STRING
+	JZ FLAG_REINICIAR
+
+	MOV DPTR, #msg_ola3
+	ACALL COMPARAR_STRING
+	JZ FLAG_OLA
+
+	MOV DPTR, #msg_clima
+	ACALL COMPARAR_STRING
+	JZ FLAG_CLIMA
+
+	ACALL ERRO_MENSAGEM
+	LJMP SALVAR_ACAO
+
+FLAG_OLA:
+	LJMP OLA
+FLAG_CLIMA:
+	LJMP CLIMA
+FLAG_REINICIAR:
+	LJMP REINICIAR
+FLAG_REPOUSO:
+	LJMP REPOUSO
+
+HORAS:
+	ACALL lcd_init
+	ACALL clearDisplay
+	ACALL delay_longo
+	MOV A, #03H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_hora
+	ACALL escreveString
+	MOV A, #42H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_hora2
+	ACALL escreveString
+	MOV R0, #30H
+	RET
+
+LUZ:
+	MOV P0, #00000000B
+	ACALL lcd_init
+	ACALL clearDisplay
+	ACALL delay_longo
+	MOV A, #02H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_ligar
+	ACALL escreveString
+	MOV A, #42H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_ok
+	ACALL escreveString
+	MOV R0, #30H
+	RET
+
+APAGAR_LUZ:
+	MOV P0, #11111111B
+	ACALL lcd_init
+	ACALL clearDisplay
+	ACALL delay_longo
+	MOV A, #00H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_desligar
+	ACALL escreveString
+	MOV R0, #30H
+	RET
+
+REPOUSO:
+	ACALL lcd_init
+	ACALL clearDisplay
+
+	ACALL delay_longo
+
+	MOV A, #01H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_dormir
+	ACALL escreveString
+	MOV A, #41H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_dormir2
+	ACALL escreveString
+
+	ACALL delay
+	ACALL clearDisplay
+	ACALL delay_longo
+
+	MOV A, #01H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_dormir3
+	ACALL escreveString
+	MOV R0, #30H
+	RET
+
+REINICIAR:
+	ACALL lcd_init
+	ACALL clearDisplay
 	
+	ACALL delay_longo
+	MOV A, #01H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_reiniciar
+	ACALL escreveString
+	MOV A, #41H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_reiniciar2
+	ACALL escreveString
+
+	ACALL LIMPAR_MEMORIA
+	MOV R0, #30H
+	RET
+
+OLA:
+	ACALL lcd_init
+	ACALL clearDisplay
+
+	ACALL delay_longo
+	MOV A, #01H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_ola
+	ACALL escreveString
+	MOV A, #41H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_ola2
+	ACALL escreveString
+	MOV R0, #30H
+	RET
+
+CLIMA:
+	ACALL lcd_init
+	ACALL clearDisplay
+	ACALL MEDIR_TEMP 
+
+	ACALL delay_longo
+	
+	MOV A, #02H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_temp
+	ACALL escreveString
+
+	ACALL delay_longo
+
+	MOV A, R4            ; A = temperatura (0–255)
+	MOV B, #100          ; Dividir por 100 para obter as centenas
+	DIV AB                ; A = centenas, B = resto (dezenas e unidades)
+	ADD A, #30h           ; Converte centenas para ASCII
+	MOV R6, A             ; Armazena a centena (ASCII)
+
+	MOV A, B              ; A = resto (dezenas e unidades)
+	MOV B, #10            ; Dividir por 10 para obter as dezenas
+	DIV AB                ; A = dezenas, B = unidades
+	ADD A, #30h           ; Converte dezenas para ASCII
+	MOV R7, A             ; Armazena a dezena (ASCII)
+
+	MOV A, B              ; A = unidades
+	ADD A, #30h           ; Converte unidades para ASCII
+	MOV R5, A             ; Armazena a unidade (ASCII)
+
+	; Exibe centenas (se não for zero)
+	MOV A, #0C4h          ; Posição no LCD: linha 2, coluna 4
+	ACALL posicionaCursor
+	MOV A, R6
+	ACALL sendCharacter
+
+	; Exibe dezenas
+	MOV A, #0C5h          ; Posição no LCD: linha 2, coluna 5
+	ACALL posicionaCursor
+	MOV A, R7
+	ACALL sendCharacter
+
+	; Exibe unidades
+	MOV A, #0C6h          ; Posição no LCD: linha 2, coluna 6
+	ACALL posicionaCursor
+	MOV A, R5
+	ACALL sendCharacter
+
+	ACALL delay_longo
+	
+	MOV A, #48H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_temp2
+	ACALL escreveString
+	MOV R0, #30H
+	RET
+
+ERRO_MENSAGEM:
+	ACALL lcd_init
+	ACALL clearDisplay
+
+	ACALL delay_longo
+	MOV A, #02H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_erro
+	ACALL escreveString
+	MOV A, #41H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_erro2
+	ACALL escreveString
+	MOV R0, #30H
+	RET
+
+LIMPAR_MEMORIA:
+	INC R1
+	MOV @R1, #00H
+	CJNE R1, #50H, LIMPAR_MEMORIA
+	MOV R2, #00H
+	MOV R3, #00H
+	MOV R4, #00H
+	MOV R5, #00H
+	MOV R6, #00H
+	MOV R7, #00H
+	MOV R1, #29H
+	RET
+
 COMPARAR_STRING:
 	MOV R0, #30H
 
@@ -64,18 +290,6 @@ CONTINUAR_2:
 	SJMP COMPARAR_LOOP
 
 NAO_IGUAL:
-	SJMP $
-
-HORAS:
-	ACALL lcd_init
-	MOV A, #03H
-	ACALL posicionaCursor
-	MOV DPTR, #msg_hora
-	ACALL escreveString
-	MOV A, #42H
-	ACALL posicionaCursor
-	MOV DPTR, #msg_hora2
-	ACALL escreveString
 	RET
 	
 escreveString:
@@ -87,7 +301,22 @@ rot:
 	INC R2
 	JNZ rot ; if A is 0, then end of data has been reached - jump out of loop
 	RET
+MEDIR_TEMP:
+	CLR P3.6
+	SETB P3.6
+	JB P3.2, $
 
+	CLR P3.7
+	MOV A, P2
+	SETB P3.7
+	
+	MOV B, #2
+
+	MUL AB
+	
+	MOV R4, A
+	MOV R5, B
+	RET
 ;Sub-Rotinas -> Display
 lcd_init:
 	CLR RS		
@@ -206,27 +435,6 @@ posicionaCursor:
 	CALL delay		
 	RET
 
-retornaCursor:
-	CLR RS	      
-	CLR P1.7		
-	CLR P1.6		
-	CLR P1.5		
-	CLR P1.4		
-
-	SETB EN		
-	CLR EN		
-
-	CLR P1.7		
-	CLR P1.6		
-	SETB P1.5		
-	SETB P1.4		
-
-	SETB EN		
-	CLR EN		
-
-	CALL delay		
-	RET
-
 clearDisplay:
 	CLR RS	      
 	CLR P1.7		
@@ -253,6 +461,13 @@ delay:
 	DJNZ R0, $
 	RET
 
+delay_longo:
+	MOV R3, #100     ; 100 * delay curto (~50 * instruções)
+loop_delay:
+	ACALL delay
+	DJNZ R3, loop_delay
+	RET
+
 ;DB de pergunta
 msg_horas:   DB 'Q','u','e',' ','h','o','r','a','s',' ','s','a','o','?','$',0
 
@@ -262,9 +477,7 @@ msg_ligar_luz:   DB 'A','s','c','e','n','d','e','r',' ','a','s',' ','l','u','z',
 
 msg_apagar_luz:  DB 'A','p','a','g','a','r',' ','a','s',' ','l','u','z','e','s','$',0
 
-msg_emitir_som:  DB 'E','m','i','t','a',' ','u','m',' ','s','o','m','$',0
-
-msg_reposo:      DB 'E','n','t','r','a','r',' ','e','m',' ','m','o','d','o',' ','r','e','p','o','u','s','o','$',0
+msg_repouso_pergunta:	DB 'E','n','t','r','a','r',' ','e','m',' ','m','o','d','o',' ','r','e','p','o','u','s','o','$',0
 
 msg_reiniciar3:  DB 'R','e','i','n','i','c','i','a','r','$',0
 
@@ -275,15 +488,12 @@ msg_hora:     DB  'H','o','r','a',' ','A','t','u','a','l',':',0
 msg_hora2:    DB  '1','2',':','3','4',0
 
 msg_temp:     DB  'T','e','m','p','e','r','a','t','u','r','a',':',0
-msg_temp2:    DB  '2','5',' ','G','R','A','U','S',0
+msg_temp2:    DB  ' ','C',0
 
 msg_ligar:    DB  'L','u','z','e','s',' ','a','c','e','s','a','s','!',0
-msg_ok:       DB  'C','o','m',' ','S','u','c','e','s','s','o',0
+msg_ok:       DB  'C','o','m',' ','S','u','c','e','s','s','o','!',0
 
 msg_desligar: DB  'L','u','z','e','s',' ','D','e','s','l','i','g','a','d','a','s',0
-
-msg_som:      DB  'E','m','i','t','i','n','d','o',' ','S','o','m',':',0
-msg_som2:     DB  'A','G','U','A','R','D','E','.','.','.','.',0
 
 msg_dormir:   DB  'E','n','t','r','a','n','d','o',' ','e','m',0
 msg_dormir2:  DB  'M','o','d','o',' ','R','e','p','o','u','s','o',0
