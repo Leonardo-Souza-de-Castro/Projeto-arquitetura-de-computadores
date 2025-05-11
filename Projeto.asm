c34804b7afd911e0f9c7c69320be4144cf0b52e8
Leonardo Souza de Castro <leonardo030605@gmail.com>
2025-05-11 16:33:04 -0300

    Código finalizado

--- Projeto.asm
+++ Projeto.asm
@@ -2,7 +2,9 @@
 
 RS equ P1.3 
 EN equ P1.2
+
 ORG 0000h
+	ACALL BV_MENSAGEM
 	LJMP INICIO
 ORG 0100h
 
@@ -12,9 +14,13 @@ INICIO:
 	MOV TMOD, #20H ;CT1 no modo 2
 	MOV TH1, #243  ;valor para a recarga
 	MOV TL1, #243  ;valor para a primeira contagem
-	MOV IE,#90H ; Habilita interrupção serial
+	CLR ES ; desativa interrupção serial
+	SETB EA ; se precisar das demais
 	SETB TR1
 	MOV R0, #30H
+	MOV R1, #29H
+
+	
 
 SALVAR_ACAO:
 	JNB RI, SALVAR_ACAO
@@ -53,10 +59,6 @@ FIM:
 	ACALL COMPARAR_STRING
 	JZ APAGAR_LUZ
 
-	MOV DPTR, #msg_emitir_som
-	ACALL COMPARAR_STRING
-	JZ SOM
-
 	MOV DPTR, #msg_repouso_pergunta
 	ACALL COMPARAR_STRING
 	JZ FLAG_REPOUSO
@@ -84,6 +86,7 @@ FLAG_REINICIAR:
 	LJMP REINICIAR
 FLAG_REPOUSO:
 	LJMP REPOUSO
+	
 
 HORAS:
 	ACALL lcd_init
@@ -93,12 +96,13 @@ HORAS:
 	ACALL posicionaCursor
 	MOV DPTR, #msg_hora
 	ACALL escreveString
-	MOV A, #42H
+	MOV A, #46H
 	ACALL posicionaCursor
 	MOV DPTR, #msg_hora2
 	ACALL escreveString
 	MOV R0, #30H
-	RET
+	ACALL LIMPAR_REGISTRADORES
+	LJMP SALVAR_ACAO
 
 LUZ:
 	MOV P0, #00000000B
@@ -114,7 +118,8 @@ LUZ:
 	MOV DPTR, #msg_ok
 	ACALL escreveString
 	MOV R0, #30H
-	RET
+	ACALL LIMPAR_REGISTRADORES
+	LJMP SALVAR_ACAO
 
 APAGAR_LUZ:
 	MOV P0, #11111111B
@@ -126,27 +131,15 @@ APAGAR_LUZ:
 	MOV DPTR, #msg_desligar
 	ACALL escreveString
 	MOV R0, #30H
-	RET
-
-SOM:
-	ACALL lcd_init
-	ACALL clearDisplay
-	ACALL delay_longo
-	MOV A, #01H
-	ACALL posicionaCursor
-	MOV DPTR, #msg_som
-	ACALL escreveString
-	MOV A, #41H
-	ACALL posicionaCursor
-	MOV DPTR, #msg_som2
-	ACALL escreveString
-	MOV R0, #30H
-	RET
+	ACALL LIMPAR_REGISTRADORES
+	LJMP SALVAR_ACAO
 
 REPOUSO:
 	ACALL lcd_init
 	ACALL clearDisplay
+
 	ACALL delay_longo
+
 	MOV A, #01H
 	ACALL posicionaCursor
 	MOV DPTR, #msg_dormir
@@ -156,7 +149,7 @@ REPOUSO:
 	MOV DPTR, #msg_dormir2
 	ACALL escreveString
 
-	ACALL delay
+	ACALL delay_longo
 	ACALL clearDisplay
 	ACALL delay_longo
 
@@ -165,12 +158,13 @@ REPOUSO:
 	MOV DPTR, #msg_dormir3
 	ACALL escreveString
 	MOV R0, #30H
-	RET
+	ACALL LIMPAR_REGISTRADORES
+	LJMP SALVAR_ACAO
 
 REINICIAR:
 	ACALL lcd_init
 	ACALL clearDisplay
-
+	
 	ACALL delay_longo
 	MOV A, #01H
 	ACALL posicionaCursor
@@ -180,8 +174,15 @@ REINICIAR:
 	ACALL posicionaCursor
 	MOV DPTR, #msg_reiniciar2
 	ACALL escreveString
+
+	ACALL delay_longo
+	ACALL LIMPAR_MEMORIA
+	ACALL delay_longo
+	ACALL clearDisplay
+	ACALL delay_longo
+	ACALL BV_MENSAGEM
 	MOV R0, #30H
-	RET
+	LJMP SALVAR_ACAO
 
 OLA:
 	ACALL lcd_init
@@ -197,23 +198,66 @@ OLA:
 	MOV DPTR, #msg_ola2
 	ACALL escreveString
 	MOV R0, #30H
-	RET
+	ACALL LIMPAR_REGISTRADORES
+	LJMP SALVAR_ACAO
 
 CLIMA:
 	ACALL lcd_init
 	ACALL clearDisplay
+	ACALL MEDIR_TEMP 
 
 	ACALL delay_longo
-	MOV A, #01H
+	
+	MOV A, #02H
 	ACALL posicionaCursor
 	MOV DPTR, #msg_temp
 	ACALL escreveString
-	MOV A, #44H
+
+	ACALL delay_longo
+
+	MOV A, R4            ; A = temperatura (0–255)
+	MOV B, #100          ; Dividir por 100 para obter as centenas
+	DIV AB                ; A = centenas, B = resto (dezenas e unidades)
+	ADD A, #30h           ; Converte centenas para ASCII
+	MOV R6, A             ; Armazena a centena (ASCII)
+
+	MOV A, B              ; A = resto (dezenas e unidades)
+	MOV B, #10            ; Dividir por 10 para obter as dezenas
+	DIV AB                ; A = dezenas, B = unidades
+	ADD A, #30h           ; Converte dezenas para ASCII
+	MOV R7, A             ; Armazena a dezena (ASCII)
+
+	MOV A, B              ; A = unidades
+	ADD A, #30h           ; Converte unidades para ASCII
+	MOV R5, A             ; Armazena a unidade (ASCII)
+
+	; Exibe centenas (se não for zero)
+	MOV A, #0C4h          ; Posição no LCD: linha 2, coluna 4
+	ACALL posicionaCursor
+	MOV A, R6
+	ACALL sendCharacter
+
+	; Exibe dezenas
+	MOV A, #0C5h          ; Posição no LCD: linha 2, coluna 5
+	ACALL posicionaCursor
+	MOV A, R7
+	ACALL sendCharacter
+
+	; Exibe unidades
+	MOV A, #0C6h          ; Posição no LCD: linha 2, coluna 6
+	ACALL posicionaCursor
+	MOV A, R5
+	ACALL sendCharacter
+
+	ACALL delay_longo
+	
+	MOV A, #48H
 	ACALL posicionaCursor
 	MOV DPTR, #msg_temp2
 	ACALL escreveString
 	MOV R0, #30H
-	RET
+	ACALL LIMPAR_REGISTRADORES
+	LJMP SALVAR_ACAO
 
 ERRO_MENSAGEM:
 	ACALL lcd_init
@@ -229,6 +273,43 @@ ERRO_MENSAGEM:
 	MOV DPTR, #msg_erro2
 	ACALL escreveString
 	MOV R0, #30H
+	ACALL LIMPAR_REGISTRADORES
+	RET
+
+LIMPAR_MEMORIA:
+	INC R1
+	MOV @R1, #00H
+	CJNE R1, #50H, LIMPAR_MEMORIA
+	MOV R2, #00H
+	MOV R3, #00H
+	MOV R4, #00H
+	MOV R5, #00H
+	MOV R6, #00H
+	MOV R7, #00H
+	MOV R1, #29H
+	RET
+
+LIMPAR_REGISTRADORES:
+	MOV R2, #00H
+	MOV R3, #00H
+	MOV R4, #00H
+	MOV R5, #00H
+	MOV R6, #00H
+	MOV R7, #00H
+	RET
+
+BV_MENSAGEM:
+	ACALL lcd_init
+	ACALL clearDisplay
+
+	ACALL delay_longo
+	MOV A, #01H
+	ACALL posicionaCursor
+	MOV DPTR, #msg_bv
+	ACALL escreveString
+
+	ACALL delay_longo
+	ACALL clearDisplay
 	RET
 
 COMPARAR_STRING:
@@ -261,7 +342,22 @@ rot:
 	INC R2
 	JNZ rot ; if A is 0, then end of data has been reached - jump out of loop
 	RET
+MEDIR_TEMP:
+	CLR P3.6
+	SETB P3.6
+	JB P3.2, $
 
+	CLR P3.7
+	MOV A, P2
+	SETB P3.7
+	
+	MOV B, #2
+
+	MUL AB
+	
+	MOV R4, A
+	MOV R5, B
+	RET
 ;Sub-Rotinas -> Display
 lcd_init:
 	CLR RS		
@@ -422,9 +518,7 @@ msg_ligar_luz:   DB 'A','s','c','e','n','d','e','r',' ','a','s',' ','l','u','z',
 
 msg_apagar_luz:  DB 'A','p','a','g','a','r',' ','a','s',' ','l','u','z','e','s','$',0
 
-msg_emitir_som:  DB 'E','m','i','t','a',' ','u','m',' ','s','o','m','$',0
-
-msg_repouso_pergunta:      DB 'E','n','t','r','a','r',' ','e','m',' ','m','o','d','o',' ','r','e','p','o','u','s','o','$',0
+msg_repouso_pergunta:	DB 'E','n','t','r','a','r',' ','e','m',' ','m','o','d','o',' ','r','e','p','o','u','s','o','$',0
 
 msg_reiniciar3:  DB 'R','e','i','n','i','c','i','a','r','$',0
 
@@ -435,15 +529,12 @@ msg_hora:     DB  'H','o','r','a',' ','A','t','u','a','l',':',0
 msg_hora2:    DB  '1','2',':','3','4',0
 
 msg_temp:     DB  'T','e','m','p','e','r','a','t','u','r','a',':',0
-msg_temp2:    DB  '2','5',' ','G','R','A','U','S',0
+msg_temp2:    DB  ' ','C',0
 
 msg_ligar:    DB  'L','u','z','e','s',' ','a','c','e','s','a','s','!',0
 msg_ok:       DB  'C','o','m',' ','S','u','c','e','s','s','o','!',0
 
 msg_desligar: DB  'L','u','z','e','s',' ','D','e','s','l','i','g','a','d','a','s',0
-
-msg_som:      DB  'E','m','i','t','i','n','d','o',' ','S','o','m',':',0
-msg_som2:     DB  'A','G','U','A','R','D','E','.','.','.','.',0
 
 msg_dormir:   DB  'E','n','t','r','a','n','d','o',' ','e','m',0
 msg_dormir2:  DB  'M','o','d','o',' ','R','e','p','o','u','s','o',0
@@ -457,3 +548,7 @@ msg_ola2:       DB 'P','o','s','s','o',' ','a','j','u','d','a','r','?',0
 
 msg_erro:       DB 'N','a','o',' ','e','n','t','e','n','d','i','!',0
 msg_erro2:      DB 'F','a','l','e',' ','n','o','v','a','m','e','n','t','e',0
+
+msg_bv:       DB 'O','l','a' ,' ','B','e','m',' ','v','i','n','d','o',0
+msg_bv2:      DB 'C','o','l','o','q','u','e',' ','e','s','p','a','c','o',0
+msg_bv3:      DB 'N','o',' ','1',' ','c','o','m','a','n','d','o',0
